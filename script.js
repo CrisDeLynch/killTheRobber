df65e845c099bd6f3d1135935302f9c58dc2c049
CrisDeLynch <cpolos02@iesalbarregas.es>
2024-10-18 19:35:55 +0200

    Primera versión HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let puntuacion = 0;
+let hoyoActivo = null;
+
+
+const cuadroPuntuacion = document.getElementById('puntuacion');
+const botonEmpezar = document.getElementById('empezarJuego');
+
+
